d43681662e0278ec63de10aab4f527a8940998f2
ddd <128319891+hm1399@users.noreply.github.com>
2025-07-16 16:11:27 +0800

    11

--- map_project/map.js
+++ map_project/map.js
@@ -11,44 +11,31 @@ const cmap = new mapsvg.map(
 const existingMap = mapsvg.get(0);
 **/
 
-let scale = 1;  // 当前缩放比例
-const scaleStep = 0.05; // 每次滚动的缩放步长
-const minScale = 1;  // 最小缩放比例
-const maxScale = 3;    // 最大缩放比例
 
-const map = document.getElementById('map'); // 获取SVG地图
+
+
 const mapContainer = document.getElementById('map-container'); // 获取地图容器
-
+const textbox = document.getElementById('region-name'); //text box 容器
 // 获取所有的区域（path）
 const regions = document.querySelectorAll('#hongkong-map path');
-const regionNameDisplay = document.getElementById('region-name');
 
 // 鼠标悬浮时高亮当前区域
 regions.forEach(region => {
-    region.addEventListener('mouseover', function(event) {
-        const regionName = this.getAttribute('name');  
-        regionNameDisplay.textContent = regionName;  // 更新显示的地区名
-        regionNameDisplay.style.display = 'block';    // 显示区域名称
-
-        // 根据鼠标位置调整文本框位置
-        const mouseX = event.pageX; // 鼠标X坐标
-        const mouseY = event.pageY; // 鼠标Y坐标
-        console.clear(); // 清除控制台
-        console.log(`X: ${event.clientX}, Y: ${event.clientY}`); 
-
-        regionNameDisplay.style.left = mouseX-350 + 'px';  // 这个可以调整弹出来的文本位置，相对于鼠标位置
-        regionNameDisplay.style.top = mouseY-300 + 'px';   
-
-        this.classList.add('highlight');  // 高亮当前区域
-                
-
+    //鼠标移动事件
+    region.addEventListener('mouseover',function(event){
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        textbox.style.left = mouseX+'px';
+        textbox.style.top= mouseY+'px';
+        const name=region.getAttribute("name");
+        textbox.textContent = name;
+        textbox.style.display = 'block';
+        
     });
 
-
-    // 移除高亮
     region.addEventListener('mouseout', function() {
         regionNameDisplay.style.display = 'none';     // 隐藏地区名称
-        this.classList.remove('highlight');           // 移除高亮
     });
+
 });
 
